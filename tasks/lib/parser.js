5e97744205f834b0455f73fe552482858b41e026
Chris Alden <chralden@gmail.com>
2015-03-18 10:43:59 -0400

    adding support for full query selectors

--- tasks/lib/parser.js
+++ tasks/lib/parser.js
@@ -15,14 +15,17 @@
  * Assumes file has already been checked as valid with sass binary
  */
 
+//Check if declaration string contains comment indicators
 function containsComment(declaration) {
     return (declaration.indexOf("//") !== -1 || declaration.indexOf("*/") !== -1);
 };
 
+//Add flags around comment string
 function flag(comment) {
     return '{*'+comment.replace(/\n/g, '|n')+'|n*}';
 };
 
+//Search declaration string for comment indicators and flag them if present
 function flagComments(declaration) {
     var temporarydeclaration = "",
         commentTerminated = (declaration.indexOf("/*") === -1);
@@ -54,73 +57,110 @@ function flagComments(declaration) {
 
 };
 
-module.exports = function(string) {
+var parser = {
 
-    var parents = [{}],
-        lastEnd = 0,
-        ch, i;
+    //Array to hold selectors
+    parents: [{}],
 
-    for(i = 0; i < string.length; i++){
+    //Track indeces as we walk the string
+    lastIndex: 0,
+    currentIndex: 0,
 
-        ch = string.charAt(i);
-        
-        //Ignore whitespace
-        if(ch === " "){
-            continue;
+    //Hold the current character and the full string to parse
+    currentCharacter: '',
+    parseString: '',
 
-        //If character is opening bracket, create new sub-parent
-        }else if(ch === "{"){
-            var currentparent = parents[parents.length-1];
-            var parent = flagComments(string.substring(lastEnd, i).trim());
-            
-            //Add new sub-parent to parent, and add reference in list
-            if(!currentparent[parent]) currentparent[parent] = {};
-            parents.push(currentparent[parent]); 
+    //Add a parent selector
+    addParent: function() {
+        var currentparent = this.parents[this.parents.length-1],
+            parent = this.parseString.substring(this.lastIndex, this.currentIndex).trim(),
+            endofQuery;
 
-            lastEnd = i+1;
-
-        //If character is semicolon, preceding substring is style declaration, add to parent
-        }else if(ch === ";"){
-            var currentparent = parents[parents.length-1];
-            var declaration = string.substring(lastEnd, i).trim();
-            var property, value;
-
-            //If not @ declaration, split based on :
-            if(declaration.indexOf("@") === -1){
-                declaration = declaration.split(":");
-
-            //Else calculate the index of the @ property
-            }else{
-                var splitindex = declaration.indexOf("@") + declaration.split("@")[1].indexOf(" ") + 1;
-                declaration = [declaration.substring(0, splitindex), declaration.substring(splitindex)];
-            }
-
-            property = flagComments(declaration.shift().trim());
-            value = declaration.join(':').trim();
-
-            //If property does not already exist, add it
-            if(!currentparent[property]){
-                currentparent[property] = value;
-
-            //If property does exist, add new value to array of values
-            }else{
-
-                if(Array.isArray(currentparent[property])){
-                    currentparent[property].push(value);
-                }else{
-                    currentparent[property] = [currentparent[property], value];
-                }
-            }
-            
-            lastEnd = i+1;
-
-        //If character is end bracket, remove parent from stack
-        }else if(ch === "}"){
-            parents.pop();
-            lastEnd = i+1;
+        //Account for #{} declarations
+        if(parent[parent.length-1] === '#'){
+            endofQuery = this.parseString.substring(this.currentIndex+1).indexOf('{')+1;
+            parent = this.parseString.substring(this.lastIndex, this.currentIndex + endofQuery).trim();
+            this.currentIndex = this.currentIndex + endofQuery;
         }
 
+        parent = flagComments(parent);
+        
+        //Add new sub-parent to parent, and add reference in list
+        if(!currentparent[parent]) currentparent[parent] = {};
+        this.parents.push(currentparent[parent]); 
+    },
+
+    //Add a style declaration to parent selector
+    addStyle: function() {
+        var currentparent = this.parents[this.parents.length-1],
+            declaration = this.parseString.substring(this.lastIndex, this.currentIndex).trim(),
+            property, value, splitindex;
+
+        //If not @ declaration, split based on :
+        if(declaration.indexOf("@") === -1){
+            declaration = declaration.split(":");
+
+        //Else calculate the index of the @ property
+        }else{
+            splitindex = declaration.indexOf("@") + declaration.split("@")[1].indexOf(" ") + 1;
+            declaration = [declaration.substring(0, splitindex), declaration.substring(splitindex)];
+        }
+
+        property = flagComments(declaration.shift().trim());
+        value = declaration.join(':').trim();
+
+        //If property does not already exist, add it
+        if(!currentparent[property]){
+            currentparent[property] = value;
+
+        //If property does exist, add new value to array of values
+        }else{
+
+            if(Array.isArray(currentparent[property])){
+                currentparent[property].push(value);
+            }else{
+                currentparent[property] = [currentparent[property], value];
+            }
+        }
+        
+    },
+
+    //Walk through parse string character by character
+    walkString: function(string) {
+
+        this.parseString = string;
+
+        for(this.currentIndex = 0; this.currentIndex < this.parseString.length; this.currentIndex++){
+            
+            //Set current character
+            this.currentCharacter = this.parseString.charAt(this.currentIndex);
+
+            //Ignore whitespace
+            if(this.currentCharacter === " "){
+                continue;
+
+            //If character is opening bracket, create new sub-parent
+            }else if(this.currentCharacter === "{"){
+                this.addParent();
+                this.lastIndex = this.currentIndex+1;
+
+            //If character is semicolon, preceding substring is style declaration, add to parent
+            }else if(this.currentCharacter === ";"){
+                this.addStyle();
+                this.lastIndex = this.currentIndex+1;
+
+            //If character is end bracket, remove parent from stack
+            }else if(this.currentCharacter === "}"){
+                this.parents.pop();
+                this.lastIndex = this.currentIndex+1;
+            }
+        }
+
+        return this.parents.pop();
     }
 
-    return parents.pop();
+};
+
+module.exports = function(string) {
+    return parser.walkString(string);
 };
