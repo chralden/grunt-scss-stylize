30b6d486f0141363c7606340db45ac9d6559c9f8
Chris Alden <chralden@gmail.com>
2015-04-06 14:47:06 -0400

    linting normalize functionality

--- tasks/lib/normalize.js
+++ tasks/lib/normalize.js
@@ -1,3 +1,4 @@
+'use strict';
 
 //Check if is numeric, + so that '' is considered 0, and parseInt to capture leading numbers e.g. '12px'
 var isNumeric = function(string) {
@@ -6,60 +7,61 @@ var isNumeric = function(string) {
 
 //Clean leading and trailing zeros on decimals
 var normalizeDecimals = function(value) {
-    var normalized = '';
+    var normalized = '',
         subvalues = value.split(' ');
 
-    subvalues.forEach(function(valstring, i){
+    subvalues.forEach(function(valstring, i) {
 
-      var decimal = valstring,
-          parts; 
+        var decimal = valstring,
+            parts;
 
-      if(valstring.indexOf('.') !== -1){
-          
-          parts = valstring.split('.');
+        if(valstring.indexOf('.') !== -1){
 
-          if(isNumeric(parts[0]) && isNumeric(parts[1])){
-             if(parts[0] === ''){
+            parts = valstring.split('.');
 
-                 if(parseInt(parts[1], 10) > 0){
-                     decimal = "0."+parts[1];
-                 }else{
-                     decimal = "0";
-                 }
+            if(isNumeric(parts[0]) && isNumeric(parts[1])){
+                if(parts[0] === ''){
 
-             }else if(parseInt(parts[1], 10) === 0){
-                decimal = parseInt(parts[0]) + parts[1].replace(new RegExp(parseInt(parts[1]), 'g'), "");
-             } else {
-                decimal = parseInt(parts[0]) + "." + parts[1];
-             } 
-          }
-      }
+                    if(parseInt(parts[1], 10) > 0){
+                        decimal = "0."+parts[1];
+                    }else{
+                        decimal = "0";
+                    }
 
-      normalized += decimal;
-      if(i < subvalues.length-1) normalized += ' ';
+                }else if(parseInt(parts[1], 10) === 0){
+                    decimal = parseInt(parts[0], 10) + parts[1].replace(new RegExp(parseInt(parts[1], 10), 'g'), "");
+                } else {
+                    decimal = parseInt(parts[0], 10) + "." + parts[1];
+                }
+            }
+        }
+
+        normalized += decimal;
+        if(i < subvalues.length-1) normalized += ' ';
     });
   
     return normalized;
 };
 
 var normalizeZeros = function(value) {
-  var normalized = '';
-      subvalues = value.split(' ');
+    
+    var normalized = '',
+        subvalues = value.split(' ');
 
-  subvalues.forEach(function(valstring, i){
-    var clean = valstring;
+    subvalues.forEach(function(valstring, i) {
+        var clean = valstring;
 
-    if(parseInt(clean, 10) === 0){
-      if(clean.indexOf('.') === -1){
-        clean = "0";
-      }
-    } 
+        if(parseInt(clean, 10) === 0){
+            if(clean.indexOf('.') === -1){
+                clean = "0";
+            }
+        }
 
-    normalized += clean;
-    if(i < subvalues.length-1) normalized += ' ';
-  });
+        normalized += clean;
+        if(i < subvalues.length-1) normalized += ' ';
+    });
 
-  return normalized;
+    return normalized;
 };
 
 //Normalize style values based on options
